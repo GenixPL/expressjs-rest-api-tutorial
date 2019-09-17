26e1cec8fcee33ce0e86c56cbf49dece59189eb0
genix <intergenixyt@gmail.com>
2019-09-17 13:33:46 +0200

    after second episode

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const mysql = require('mysql')
 
 app.use(morgan('dev'))
 
@@ -15,6 +16,38 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     res.send(users)
+})
+
+app.get('/user/:id', (req, res) => {
+    const connection = mysql.createConnection({
+         host: 'localhost',
+         user: 'root',
+         password: 'sample1234',
+         database: 'example_users',
+    })
+
+    const userId = req.params.id
+    // const queryString = 'SELECT * FROM users WHERE id = ' + userId // (without [] in connection.query)
+    const queryString = 'SELECT * FROM users WHERE id = ?'
+    connection.query(queryString, [userId], (err, rows, fields) => {
+        if (err) {
+            console.log(err)
+            res.sendStatus(500)
+            res.end()
+            return
+        }
+
+        const users = rows.map((row) => {
+            return {
+                NAME: row.name,
+                ID: row.id,
+            }
+        })
+
+        res.json(users) // rows
+    })
+
+    // res.end()
 })
 
 
